9aba331442c72b5f0338b2853e398c1bfdf9e2ee
Yuxin Ye <yuxin_ye@protonmail.com>
2023-05-31 15:58:24 +1000

    finish subtitles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { logo } from "../assets";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sums_logo" className="w-20 object-contain" />
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/CutieMath/summerise")}
+          className="black_btn"
+        >
+          GitHub Repo
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarise websites with <br />
+        <span className="pink_gradient">OpenAI GPT-4</span>
+      </h1>
+    </header>
+  );
 };
 
 export default Hero;
