bc027678be3c921ff5c677e368fd50abe5951241
Yuxin Ye <yuxin_ye@protonmail.com>
2023-05-31 15:39:57 +1000

    add hero section and adjust background

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import Hero from "./components/Hero";
+import Demo from "./components/Demo";
+import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+    </main>
+  );
 };
 
 export default App;
