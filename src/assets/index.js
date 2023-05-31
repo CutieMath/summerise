7e491d16a8adbec699d9ad5f1ee273f1e1952c3a
Yuxin Ye <yuxin_ye@protonmail.com>
2023-05-31 17:19:02 +1000

    finish adding input style and state

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,6 @@ import loader from "./loader.svg";
 import copy from "./copy.svg";
 import logo from "./crystal-ball-128.png";
 import tick from "./tick.svg";
+import submitArrow from "./right-arrow.png";
 
-export { linkIcon, loader, copy, logo, tick };
+export { linkIcon, loader, copy, logo, tick, submitArrow };
--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick, submitArrow } from "../assets";
 
 const Demo = () => {
-  return <div>Demo</div>;
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async (e) => {
+    alert("Submitted");
+  };
+
+  const handleInputChange = (e) => {
+    console.log(e.target.value);
+    setArticle({ ...article, url: e.target.value });
+  };
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        {/* input form  */}
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) => handleInputChange(e)}
+            required
+            className="url_input peer"
+          />
+          <button
+            type="submit"
+            className="submit_btn
+            peer-focus:border-gray-700 peer-focus:text-gray-700
+          "
+          >
+            <img src={submitArrow} alt="send_icon" className="w-4" />
+          </button>
+        </form>
+
+        {/* url history  */}
+
+        {/* results display  */}
+      </div>
+    </section>
+  );
 };
 
 export default Demo;
