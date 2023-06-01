f44f09cd9fffbffe9f762ef691b961b336ebed5c
Yuxin Ye <yuxin_ye@protonmail.com>
2023-06-01 14:34:35 +1000

    display search history

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -8,14 +8,30 @@ const Demo = () => {
     summary: "",
   });
 
+  const [allArticles, setAllArticles] = useState([]);
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { data } = await getSummary({ articleUrl: article.url });
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
       setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      // Update the articles in local storage
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
       console.log(newArticle);
     }
   };
@@ -56,6 +72,26 @@ const Demo = () => {
         </form>
 
         {/* url history  */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => {
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img
+                  src={copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>;
+          })}
+        </div>
 
         {/* results display  */}
       </div>
